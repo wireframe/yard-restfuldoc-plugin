0070c420e57472449629a6981712f3227eb32901
Ryan Sonnek <ryan@socialcast.com>
2012-03-26 20:58:56 -0500

    update plugin

---
+++ example/foo_controller.rb
@@ -0,0 +1,15 @@
+class FooController
+  ##
+  # Returns all foos.
+  # 
+  # @url [GET] /foos.[format]?[arguments]
+  def index
+  end
+
+  ##
+  # Creates a new foo.
+  # 
+  # @url [POST] /foos.[format]?[arguments]
+  def create
+  end
+end
