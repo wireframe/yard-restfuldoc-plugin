2aa861f5660e4d0577a98bec449c0e2063d55695
Ryan Sonnek <ryan@socialcast.com>
2012-03-26 23:15:10 -0500

    add more examples

--- example/foo_controller.rb
+++ example/foo_controller.rb
@@ -5,7 +5,7 @@ class FooController
   # @url [GET] /foos
   #
   # @param [String] bar only return objects with certain properties
-  # @parameter [Integer] baz this does something too
+  # @param [Integer] baz this does something too
   #
   # @return 401 if unauthorized
   def index
@@ -16,9 +16,29 @@ class FooController
   # 
   # @url [POST] /foos
   #
-  # @parameter [String] foo[name] name for the new foo
-  # @parameter [String] foo[description] full description
-  # @parameter [Date] foo[active_at] date to start
+  # @param [String] foo[name] name for the new foo
+  # @param [String] foo[description] full description
+  # @param [Date] foo[active_at] date to start
   def create
+  end
+
+  ##
+  # Lookup an instance of foo.
+  #
+  # @url [GET] /foos/:id
+  #
+  # @param [Integer] :id unique identifier to lookup
+  #
+  # @example standard JSON response
+  #   {
+  #     'foo': {
+  #       'id': 123,
+  #       'name': 'example',
+  #       'description: 'foo bar'
+  #     }
+  #   }
+  #
+  # @return 404 if not found
+  def show
   end
 end
